dd878c6b177f485c9b8e59956bf85e06a2adf130
Tamarakuro Joseph Aduwari <tamarakuro.aduwari@student.adelaide.edu.au>
2022-02-26 13:39:16 +1030

    layouts added

---
+++ live_accident_reporting/src/components/Sections/Body.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <p>Hello world</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BodySection",
+  components: {},
+};
+</script>
+
+<style lang="css" scoped></style>
