000decba059909319ebd664282fc70aef6ff6614
YvonneTansu <yvonne.tansu@gmail.com>
2022-02-26 14:43:02 +1030

    feat: accident reporting modal

--- live_accident_reporting/src/components/HelloWorld.vue
+++ live_accident_reporting/src/components/HelloWorld.vue
@@ -1,40 +1,30 @@
-<script setup>
-import { ref } from 'vue'
+<!--
+Modal component with customizable slots and CSS transitions.
+-->
 
-defineProps({
-  msg: String
-})
+<script>
+import Modal from './Modal.vue'
 
-const count = ref(0)
+export default {
+  components: {
+    Modal
+  },
+  data() {
+    return {
+      showModal: false
+    }
+  }
+}
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
+<div>
+  <button id="show-modal" @click="showModal = true">Show Modal</button>
 
-  <p>
-    Recommended IDE setup:
-    <a href="https://code.visualstudio.com/" target="_blank">VSCode</a>
-    +
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-  </p>
-
-  <p>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">
-      Vite Documentation
-    </a>
-    |
-    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
-  </p>
-
-  <button type="button" @click="count++">count is: {{ count }}</button>
-  <p>
-    Edit
-    <code>components/HelloWorld.vue</code> to test hot module replacement.
-  </p>
+  <Teleport to="body">
+    <!-- use the modal component, pass in the prop -->
+    <modal :show="showModal" @close="showModal = false">
+    </modal>
+  </Teleport>
+  </div>
 </template>
-
-<style scoped>
-a {
-  color: #42b983;
-}
-</style>
